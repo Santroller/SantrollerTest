7334bd976d0c4c815629eac72810bdc97f6c490e
Sanjay Govind <sanjay.govind9@gmail.com>
2026-02-19 16:27:02 +1300

    build uf2

--- src/pages/Connect.page.tsx
+++ src/pages/Connect.page.tsx
@@ -1,12 +1,14 @@
 import { IconExclamationCircle } from '@tabler/icons-react';
-import { Alert, Button } from '@mantine/core';
+import { Alert, Button, Space, Text } from '@mantine/core';
 import { Layout } from '@/components/Layout/Layout';
 import { useConfigStore } from '@/components/SettingsContext/SettingsContext';
+import { useTranslation } from 'react-i18next';
 
 export function ConnectPage() {
   const connect = useConfigStore((state) => state.connect);
   const disconnect = useConfigStore((state) => state.disconnect);
   const connected = useConfigStore((state) => state.connected);
+  const { t } = useTranslation();
   return (
     <>
       <Layout>
@@ -24,6 +26,13 @@ export function ConnectPage() {
           <Button onClick={disconnect}>Disconnect from Santroller</Button>
         )}
         {navigator.hid && !connected && <Button onClick={connect}>Connect to Santroller</Button>}
+        <Space h="md" />
+        <Text size="h1">{t('getting_started.title')}</Text>
+        <Text size="sm">{t('getting_started.text')}</Text>
+        <Space h="md" />
+        <Button component="a" download="santroller.uf2" href="santroller.uf2" target="_blank">
+          Download uf2
+        </Button>
       </Layout>
     </>
   );
